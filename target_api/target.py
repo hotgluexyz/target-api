f33ff79ff5b42777bcb2247adaa77f122353dae6
Keyna Rafael <95432445+keyn4@users.noreply.github.com>
2023-12-14 10:53:39 -0500

    add support to send batches (#7)

--- target_api/target.py
+++ target_api/target.py
@@ -1,26 +1,28 @@
 """Api target class."""
 
 from __future__ import annotations
+
 from typing import Type
 
-from singer_sdk import Sink, typing as th
-from singer_sdk.target_base import Target
+from singer_sdk import Sink
 from target_hotglue.target import TargetHotglue
 
-from target_api.sinks import (
-    ApiSink,
-)
+from target_api.sinks import BatchSink, RecordSink
 
 
 class TargetApi(TargetHotglue):
     """Sample target for Api."""
 
     name = "target-api"
-    SINK_TYPES = [ApiSink]
+    SINK_TYPES = [RecordSink, BatchSink]
     MAX_PARALLELISM = 10
+    target_counter = {}
 
     def get_sink_class(self, stream_name: str) -> Type[Sink]:
-        return ApiSink
+        if self.config.get("process_as_batch"):
+            return BatchSink
+        return RecordSink
+
 
 if __name__ == "__main__":
     TargetApi.cli()
