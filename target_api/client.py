b118c8c98eebb08cf11b631a4a15cfa4cf76e4bc
Davi Souza <ddas.souza@gmail.com>
2024-09-12 18:37:25 -0300

    HG-3433: adds 'custom_headers' (#18)

--- target_api/client.py
+++ target_api/client.py
@@ -58,6 +58,22 @@ class ApiSink(HotglueBaseSink):
     def unified_schema(self) -> BaseModel:
         return None
 
+    @property
+    def custom_headers(self) -> dict:
+        custom_headers = {
+            "User-Agent": self._config.get("user_agent", "target-api <hello@hotglue.xyz>")
+        }
+        config_custom_headers = self._config.get("custom_headers") or list()
+        for ch in config_custom_headers:
+            if not isinstance(ch, dict):
+                continue
+            name = ch.get("name")
+            value = ch.get("value")
+            if not isinstance(name, str) or not isinstance(value, str):
+                continue
+            custom_headers[name] = value
+        return custom_headers
+        
     def response_error_message(self, response: requests.Response) -> str:
         try:
             response_text = f" with response body: '{response.text}'"
