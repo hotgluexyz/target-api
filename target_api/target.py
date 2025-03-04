bccbf0919e433bfec4e806576f98ab9ee4853600
Key <keynarafaelp@gmail.com>
2025-03-04 09:03:37 -0500

    post empty records if post_empty_record flag is on

--- target_api/target.py
+++ target_api/target.py
@@ -31,33 +31,36 @@ class TargetApi(TargetHotglue):
         Args:
             sink: Sink to be drained.
         """
-
-        draining_status = sink.start_drain()
-        # if there is schema but no records for a sink, post an empty record
-        if not draining_status and (
-            not sink.latest_state
-            or not sink.latest_state.get("summary", {}).get(sink.name)
-        ):
-            draining_status = {"records": [{}]}
-            sink.send_empty_record = True
-        elif not sink or sink.current_size == 0:
-            return
-
-        # send an empty record for batchSink
-        if self.config.get("process_as_batch"):
-            sink.process_batch(draining_status)
-            sink.mark_drained()
-        # send an empty record and update state for single record Sink
+        # post empty records only if post_empty_record flag is set as True (it's False by default)
+        if not self.config.get("post_empty_record", False):
+            super().drain_one(sink)
+        
         else:
-            sink.process_record({"id": ""}, {})
-            if not self._latest_state:
-                # If "self._latest_state" is empty, save the value of "sink.latest_state"
-                self._latest_state = sink.latest_state
+            draining_status = sink.start_drain()
+            # if there is schema but no records for a sink, post an empty record
+            if not draining_status and (
+                not sink.latest_state
+                or not sink.latest_state.get("summary", {}).get(sink.name)
+            ):
+                draining_status = {"records": [{}]}
+                sink.send_empty_record = True
+
+            
+            # send an empty record for batchSink
+            if self.config.get("process_as_batch"):
+                sink.process_batch(draining_status)
+                sink.mark_drained()
+            # send an empty record and update state for single record Sink
             else:
-                for key in self._latest_state.keys():
-                    sink_latest_state = sink.latest_state or dict()
-                    self._latest_state[key].update(sink_latest_state.get(key) or dict())
-            self._write_state_message(self._latest_state)
+                sink.process_record({}, {})
+                if not self._latest_state:
+                    # If "self._latest_state" is empty, save the value of "sink.latest_state"
+                    self._latest_state = sink.latest_state
+                else:
+                    for key in self._latest_state.keys():
+                        sink_latest_state = sink.latest_state or dict()
+                        self._latest_state[key].update(sink_latest_state.get(key) or dict())
+                self._write_state_message(self._latest_state)
 
 
 if __name__ == "__main__":
